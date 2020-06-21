c9d498f6b3e711fa2b77794ec85b2d363bb3b7bc
Jonathan Cote <jonathancote92@gmail.com>
2020-06-20 22:51:12 -0500

    client side

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  "mongoURI": "mongodb+srv://test-stuff:bicpow@cluster0-3n5il.gcp.mongodb.net/mern-starter?retryWrites=true&w=majority"
+}
--- server.js
+++ server.js
@@ -1,6 +1,34 @@
-import app from './app';
-import config from './config';
+// import app from './app';
+// import config from './config';
 
-const { PORT } = 5000;
+// const { PORT } = config;
 
-app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+// app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+
+// old super simple server.js code 
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const items = require('./routes/api/items');
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+mongoose
+  .connect(db, {useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+// Use Routes
+app.use('/api/items', items);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
