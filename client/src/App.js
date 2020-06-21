4baee3976b102056d1dc563a38e549bc0087960d
Jonathan Cote <jonathancote92@gmail.com>
2020-06-21 14:26:30 -0500

    added redux

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
+import { Provider } from 'react-redux';
+import store from './store';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       <AppNavbar />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+         <AppNavbar />
+         <ShoppingList />
+        </div>      
+      </Provider>
+
     );    
   }
 }
